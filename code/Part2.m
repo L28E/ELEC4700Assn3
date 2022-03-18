e1d98777323af97b3b36f08ce6fae98f1b689287
Nick <nick@pop-os.localdomain>
2022-03-18 18:53:22 -0400

    Fixed logical indexingin 2C

--- code/Part2.m
+++ code/Part2.m
@@ -1,108 +1,115 @@
-close all;
-clear;
+function [Ex,Ey,nx,ny] = Part2(main,w,l,dx,dy)
+% Computes the E field of the bottleneck scenario.
+% example call: Part2(true,100e-9,200e-9,25e-10,25e-10) 
 
-w=100e-9;
-l=200e-9;
-dx=25e-10;
-dy=25e-10;
-ny=int64(w/dx);
-nx=int64(l/dy);
+    %w=100e-9;
+    %l=200e-9;
+    %dx=25e-10;
+    %dy=25e-10;
+    ny=int64(w/dx);
+    nx=int64(l/dy);
 
-V0=0.1;
-cMap=ones(nx,ny);
-% Change the conductance for the middle third on each side 
-cMap(nx/3:2*nx/3  , 1:ny/3)=1e-15; 
-cMap(nx/3:2*nx/3 , 2*ny/3:ny)=1e-15;
+    V0=0.1;
+    cMap=ones(nx,ny);
+    % Change the conductance for the bottleneck on each side 
+    cMap(nx/3:2*nx/3  , 1:ny/3)=1e-2; 
+    cMap(nx/3:2*nx/3 , 2*ny/3:ny)=1e-2;
 
-% G-matrix, relates the value of a node to all other nodes
-G=sparse(nx*ny,nx*ny);
-% F-vector, the boundary conditons
-F = sparse(nx*ny,1);
+    % G-matrix, relates the value of a node to all other nodes
+    G=sparse(nx*ny,nx*ny);
+    % F-vector, the boundary conditons
+    F = sparse(nx*ny,1);
 
-% Populate G matrix
-% Code from https://github.com/L28E/4700Code/blob/master/CondCode/GetCurrents.m
-for i = 1:nx
-    for j = 1:ny
-        n = j + (i - 1) * ny;
+    % Populate G matrix
+    % Code from https://github.com/L28E/4700Code/blob/master/CondCode/GetCurrents.m
+    for i = 1:nx
+        for j = 1:ny
+            n = j + (i - 1) * ny;
 
-        if i == 1
-            G(n, :) = 0;
-            G(n, n) = 1;
-            F(n)=V0;
-        elseif i == nx
-            G(n, :) = 0;
-            G(n, n) = 1;
-        elseif j == 1
-            nxm = j + (i - 2) * ny;
-            nxp = j + (i) * ny;
-            nyp = j + 1 + (i - 1) * ny;
+            if i == 1
+                G(n, :) = 0;
+                G(n, n) = 1;
+                F(n)=V0;
+            elseif i == nx
+                G(n, :) = 0;
+                G(n, n) = 1;
+            elseif j == 1
+                nxm = j + (i - 2) * ny;
+                nxp = j + (i) * ny;
+                nyp = j + 1 + (i - 1) * ny;
 
-            rxm = (cMap(i, j) + cMap(i - 1, j)) / 2.0;
-            rxp = (cMap(i, j) + cMap(i + 1, j)) / 2.0;
-            ryp = (cMap(i, j) + cMap(i, j + 1)) / 2.0;
+                rxm = (cMap(i, j) + cMap(i - 1, j)) / 2.0;
+                rxp = (cMap(i, j) + cMap(i + 1, j)) / 2.0;
+                ryp = (cMap(i, j) + cMap(i, j + 1)) / 2.0;
 
-            G(n, n) = -(rxm+rxp+ryp);
-            G(n, nxm) = rxm;
-            G(n, nxp) = rxp;
-            G(n, nyp) = ryp;
+                G(n, n) = -(rxm+rxp+ryp);
+                G(n, nxm) = rxm;
+                G(n, nxp) = rxp;
+                G(n, nyp) = ryp;
 
-        elseif j ==  ny
-            nxm = j + (i - 2) * ny;
-            nxp = j + (i) * ny;
-            nym = j - 1 + (i - 1) * ny;
+            elseif j ==  ny
+                nxm = j + (i - 2) * ny;
+                nxp = j + (i) * ny;
+                nym = j - 1 + (i - 1) * ny;
 
-            rxm = (cMap(i, j) + cMap(i - 1, j)) / 2.0;
-            rxp = (cMap(i, j) + cMap(i + 1, j)) / 2.0;
-            rym = (cMap(i, j) + cMap(i, j - 1)) / 2.0;
+                rxm = (cMap(i, j) + cMap(i - 1, j)) / 2.0;
+                rxp = (cMap(i, j) + cMap(i + 1, j)) / 2.0;
+                rym = (cMap(i, j) + cMap(i, j - 1)) / 2.0;
 
-            G(n, n) = -(rxm + rxp + rym);
-            G(n, nxm) = rxm;
-            G(n, nxp) = rxp;
-            G(n, nym) = rym;
-        else
-            nxm = j + (i-2)*ny;
-            nxp = j + (i)*ny;
-            nym = j-1 + (i-1)*ny;
-            nyp = j+1 + (i-1)*ny;
+                G(n, n) = -(rxm + rxp + rym);
+                G(n, nxm) = rxm;
+                G(n, nxp) = rxp;
+                G(n, nym) = rym;
+            else
+                nxm = j + (i-2)*ny;
+                nxp = j + (i)*ny;
+                nym = j-1 + (i-1)*ny;
+                nyp = j+1 + (i-1)*ny;
 
-            rxm = (cMap(i,j) + cMap(i-1,j))/2.0;
-            rxp = (cMap(i,j) + cMap(i+1,j))/2.0;
-            rym = (cMap(i,j) + cMap(i,j-1))/2.0;
-            ryp = (cMap(i,j) + cMap(i,j+1))/2.0;
+                rxm = (cMap(i,j) + cMap(i-1,j))/2.0;
+                rxp = (cMap(i,j) + cMap(i+1,j))/2.0;
+                rym = (cMap(i,j) + cMap(i,j-1))/2.0;
+                ryp = (cMap(i,j) + cMap(i,j+1))/2.0;
 
-            G(n,n) = -(rxm+rxp+rym+ryp);
-            G(n,nxm) = rxm;
-            G(n,nxp) = rxp;
-            G(n,nym) = rym;
-            G(n,nyp) = ryp;
+                G(n,n) = -(rxm+rxp+rym+ryp);
+                G(n,nxm) = rxm;
+                G(n,nxp) = rxp;
+                G(n,nym) = rym;
+                G(n,nyp) = ryp;
+            end
+
         end
-
     end
+
+    % Solve for voltage
+    V = G\F;
+    for x=1:nx
+        for y=1:ny
+          n = y + (x - 1) * ny;
+          v_surf(x,y)=V(n);
+        end      
+    end
+    
+    [Ex,Ey]=gradient(-v_surf',dx,dy);    
+    
+    if main==true
+        [X,Y]=meshgrid(linspace(0,l,nx),linspace(0,w,ny));
+
+        % Voltage Plot 
+        figure(1);
+        surf(X,Y,v_surf','EdgeColor','none');
+        title('V(x,y)');
+        ylabel('W');
+        xlabel('L');
+
+        % E field Plot 
+
+        figure(2);
+        quiver(X,Y,Ex,Ey);
+        title('E(x,y)' );
+        ylabel('W');
+        xlabel('L');        
+    end    
+   
 end
-
-% Solve for voltage
-V = G\F;
-for x=1:nx
-    for y=1:ny
-      n = y + (x - 1) * ny;
-      v_surf(x,y)=V(n);
-    end      
-end 
-
-[X,Y]=meshgrid(linspace(0,l,nx),linspace(0,w,ny));
-
-% Voltage Plot 
-figure(1);
-surf(X,Y,v_surf','EdgeColor','none');
-title('V(x,y)');
-ylabel('W');
-xlabel('L');
-
-% E field Plot 
-[Ex,Ey]=gradient(-v_surf',dx,dy);
-figure(2);
-quiver(X,Y,Ex,Ey);
-title('E(x,y)' );
-ylabel('W');
-xlabel('L');
 
--- code/Part2C.m
+++ code/Part2C.m
@@ -7,36 +7,44 @@
 clear
 close all
 
-% GET FIELD FROM PART 2
-Part2
+type=0;                 % 0 for specular, anything else for diffusive 
 
 m_0=9.10938e-31;        % electron rest mass (kg)
 m_n=0.26*m_0;           % electron effective mass (kg)
 T=300;                  % Temperature (K)
 k_b=1.380649e-23;       % Boltzmann Constant (J/K)
+tau_mn=0.2e-12;         % Mean time between collisions 
 q = 1.60217653e-19;     % Charge of an electron
-
-V_th=sqrt(2*k_b*T/m_n);   % Thermal velocity (m/s)
-tau_mn=0.2e-12; % Mean time between collisions 
 
 num_electrons=1000;
 num_steps=1000;
-num_traces=10; % Increase the number of tracked electrons for MFP accuracy
+num_traces=8;
 ymax=100e-9;
 xmax=200e-9;
-area=xmax*ymax; % Cross sectional area, for the current calculation
 dt=4e-15;
 P_scat=1-exp(-dt/tau_mn);
+
+% Generate the boxes
+box_x=0.8e-7;
+box_width=0.4e-7;
+box_right=box_x+box_width;
+box_y=0;
+box_height=0.4e-7;
+box_top=box_y+box_height;
+box_bottom=0.6e-7;
 
 % Generate random electron positions
 Px=rand(1,num_electrons).*xmax;
 Py=rand(1,num_electrons).*ymax;
 
+% Remove electrons from the boxes
+oob=get_oob(Px,Py,box_x,box_right,box_top,box_bottom);
+[M,I]=get_nearest_bound(num_electrons,Px,Py,oob,box_x,box_right,box_top,box_bottom);
+[Px,Py]=remove_oob(Px,Py,oob,box_x,box_right,box_top,box_bottom,M,I);
+
 % Generate random electron velocities (Normal distribution for each component of velocity)
-%twiddle=1.25;  
-twiddle=1; 
-Vx=randn(1,num_electrons)*sqrt(k_b*T/m_n)*twiddle;
-Vy=randn(1,num_electrons)*sqrt(k_b*T/m_n)*twiddle;
+Vx=randn(1,num_electrons)*sqrt(k_b*T/m_n);
+Vy=randn(1,num_electrons)*sqrt(k_b*T/m_n);
 
 % Randomly select some electrons to follow
 tracked_indices=randperm(num_electrons,num_traces);
@@ -45,12 +53,16 @@ tracked_indices=randperm(num_electrons,num_traces);
 X=zeros(num_traces,num_steps);
 Y=zeros(num_traces,num_steps);
 t=zeros(1,num_steps);
-current=zeros(1,num_steps);
 
 % 2D array to track the timesteps where each electron has a collision
 collisions=zeros(num_electrons,num_steps);
 
-figure(1)
+% Get the Electric field using finite difference method
+dx=25e-10;
+dy=dx;
+[Ex,Ey,nx,ny]=Part2(false,ymax,xmax,dx,dy);
+
+figure(1);
 
 for k=2:num_steps
     % Update positions
@@ -59,24 +71,22 @@ for k=2:num_steps
     
     % Scatter electrons
     scat=rand(1,num_electrons)<P_scat;
-    Vx(scat)=randn(1,length(Vx(scat)))*sqrt(k_b*T/m_n)*twiddle;
-    Vy(scat)=randn(1,length(Vx(scat)))*sqrt(k_b*T/m_n)*twiddle;    
+    Vx(scat)=randn(1,length(Vx(scat)))*sqrt(k_b*T/m_n);
+    Vy(scat)=randn(1,length(Vx(scat)))*sqrt(k_b*T/m_n);    
     
     % TODO: Try to optimize this    
     %=================================================    
     % Force is a function of the E-field, which is a function of space because of the bottleneck.
     % Select electrons by E-Field bin and apply force accordingly.
     
-    % For all electrons in a dx*dy bin, apply the force of that bin
+    % For all electrons in a dx*dy bin, apply the force of that bin       
     for x=1:nx
         for y=1:ny
-          n = y + (x - 1) * ny;          
-          Vx(Px>(x-1)*dx & Px<x*dx & Py>(y-1)*dy & Py<y*dy)=Vx(Px>(x-1)*dx & Px<x*dx & Py>(y-1)*dy & Py<y*dy)+q*Ex(n)/m_n*dt;
-          Vy(Px>(x-1)*dx & Px<x*dx & Py>(y-1)*dy & Py<y*dy)=Vy(Px>(x-1)*dx & Px<x*dx & Py>(y-1)*dy & Py<y*dy)+q*Ey(n)/m_n*dt;
+            Vx(Px>(double(x)-1)*dx & Px<double(x)*dx & Py>(double(y)-1)*dy & Py<double(y)*dy)=Vx(Px>(double(x)-1)*dx & Px<double(x)*dx & Py>(double(y)-1)*dy & Py<double(y)*dy)+q*Ex(y,x)/m_n*dt;
+            Vy(Px>(double(x)-1)*dx & Px<double(x)*dx & Py>(double(y)-1)*dy & Py<double(y)*dy)=Vy(Px>(double(x)-1)*dx & Px<double(x)*dx & Py>(double(y)-1)*dy & Py<double(y)*dy)+q*Ey(y,x)/m_n*dt;            
         end      
-    end
+    end    
     %=================================================
-    
     
     % Electrons leaving lateral bounds come back in to preserve density
     Px(Px<0)=xmax+Px(Px<0);
@@ -87,54 +97,62 @@ for k=2:num_steps
     beyond_lower=Py<0;
     Vy(beyond_lower|beyond_upper)=-Vy(beyond_lower|beyond_upper);
     Py(beyond_lower)=-Py(beyond_lower);
-    Py(beyond_upper)=-Py(beyond_upper)+2*ymax;    
+    Py(beyond_upper)=-Py(beyond_upper)+2*ymax;  
+    
+    % Logically index electrons which are out of bounds
+    oob=get_oob(Px,Py,box_x,box_right,box_top,box_bottom);    
+
+    % Determine which bound each oob electron is closest to
+    [M,I]=get_nearest_bound(num_electrons,Px,Py,oob,box_x,box_right,box_top,box_bottom);  
+    
+    hzn_flip=oob & M'~=0 & (I'==1|I'==2); % For oob electrons nearest the left or right edges, flip their horizontal velocity
+    vert_flip=oob & M'~=0 & (I'==3|I'==4); % For oob electrons nearest the top boundary, flip their vertical velocity
+    
+    % Reflect & remove oob electrons from boxes
+    if type==0          
+        Vx(hzn_flip)=-Vx(hzn_flip);          
+        Vy(vert_flip)=-Vy(vert_flip);
+    else
+        Vx(hzn_flip)=-sign(Vx(hzn_flip)).*abs(randn(1,nnz(hzn_flip))*sqrt(k_b*T/m_n));        
+        Vy(vert_flip)=-sign(Vy(vert_flip)).*abs(randn(1,nnz(vert_flip))*sqrt(k_b*T/m_n));
+    end
+    [Px,Py]=remove_oob(Px,Py,oob,box_x,box_right,box_top,box_bottom,M,I);   
     
     % Update the tracked electrons and temperature 
     t(k)=t(k-1)+dt;
     X(:,k)=Px(tracked_indices);
     Y(:,k)=Py(tracked_indices);    
-    current(k)=mean(Vx) *q *10e19 ... % Current Density 
-               *area; % Cross sectional area
-    
+        
     % Record the time steps where the electrons scatter
     %collisions(scat|beyond_upper|beyond_lower,k)=1;
     collisions(scat,k)=1;
     
-    % Plot electron trajectories        
+    % Plot electron trajectories   
     plot(X(1,1:k),Y(1,1:k),".",X(2,1:k),Y(2,1:k),".",X(3,1:k),Y(3,1:k),".",...
-        X(4,1:k),Y(4,1:k),".",X(5,1:k),Y(5,1:k),".",'MarkerSize',4) % hide the discontinuities by using dots :)
-       
+        X(4,1:k),Y(4,1:k),".",X(5,1:k),Y(5,1:k),".",X(6,1:k),Y(6,1:k),".",...
+        X(7,1:k),Y(7,1:k),".",X(8,1:k),Y(8,1:k),".",'MarkerSize',4) % hide the discontinuities by using dots :)
+    hold on;
+    
+    rectangle('Position',[box_x box_y box_width box_height]);
+    rectangle('Position',[box_x box_bottom box_width box_height]);
+    hold off;
    
     pause(0.00001)    
 end
-title("I: Electron Trajectories")
-ylabel("Y")
-xlabel("X")
 
-% Plot current
-figure(2)
-plot(t,current)
-title("Current vs. Time")
-ylabel("Current (A)")
-xlabel("Time (s)")
+function oob = get_oob(Px,Py,left,right,top,bottom)
+    oob=(Px>=left & Px<=right & Py<=top)|(Px>=left & Px<=right & Py>=bottom);
+end
 
-% Plot temperature
-%https://www.mathworks.com/help/matlab/ref/griddata.html
-figure(3);
-temp=(Vx(:).^2+Vy(:).^2)*m_n/k_b/2;
-[xq,yq] = meshgrid(0:0.05*xmax:xmax,0:0.05*ymax:ymax);
-vq = griddata(Px,Py,temp',xq,yq);
-s=mesh(xq,yq,vq);
-hold on;
-plot3(Px,Py,temp','.r');
-colorbar
-title("Temperature Map")
-s.FaceColor='interp';
-%colormap('parula')
+function [minimum,index] = get_nearest_bound(num_electrons, Px,Py,oob,left,right,top,bottom)
+    diffs=zeros(num_electrons,4);
+    diffs(oob,:)=[abs(Px(oob)'-left) abs(Px(oob)'-right) abs(Py(oob)'-top) abs(Py(oob)'-bottom)];
+    [minimum,index]=min(diffs,[],2);
+end
 
-% Plot density map
-figure(4);
-binscatter(Px,Py,75)
-title("Electron Density Map")
-axis([0 xmax 0 ymax])
-colormap('parula')
+function [Px,Py] = remove_oob(Px,Py,oob,left,right,top,bottom,M,I)
+    Px(oob & M'~=0 & I'==1)=2*left-Px(oob & M'~=0 & I'==1);
+    Px(oob & M'~=0 & I'==2)=2*right-Px(oob & M'~=0 & I'==2);
+    Py(oob & M'~=0 & I'==3)=2*top-Py(oob & M'~=0 & I'==3);
+    Py(oob & M'~=0 & I'==4)=2*bottom-Py(oob & M'~=0 & I'==4);
+end
